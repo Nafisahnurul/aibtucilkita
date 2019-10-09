c42dd880dee77d666114a635b6a7482df12e066c
nafisahnurul <nafisah.nurul15@gmail.com>
2019-10-09 18:30:40 +0700

    unsigning port 9

--- server.js
+++ server.js
@@ -4,6 +4,33 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
+const getHomePage = require('./routes/index.js');
+const getSearch = require('./routes/search.js');
+
+// configure middleware
+app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
+app.set('view engine', 'ejs'); // configure template engine
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // parse form data client
+app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
+app.use(fileUpload()); // configure fileupload
+
+// routes for the app
+
+app.use('/', getHomePage);
+app.use('/search',getSearch);
+
+// set the app to listen on the port
+app.listen(process.env.PORT || 5000, function (req, res) {
+    console.log(`Server running`);
+});
+/*
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
  const getHomePage = require('./routes/index.js');
  const getSearch = require('./routes/search.js');
 
@@ -26,3 +53,4 @@ app.use('/search',getSearch);
 app.listen(process.env.PORT, function (req, res) {
     console.log("App is running");
 })
+*/
